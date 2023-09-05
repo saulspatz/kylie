9e66aa2c9f84644035cbcec968fa78882b084f7e
Saul Spatz <saul.spatz@gmail.com>
2023-09-05 15:12:02 -0500

    generate.sh

---
+++ generate.sh
@@ -0,0 +1,19 @@
+./keen --generate 100 6de > keys/easy6.txt &
+./keen --generate 100 7de > keys/easy7.txt &
+./keen --generate 100 8de > keys/easy8.txt &
+./keen --generate 100 9de > keys/easy9.txt &
+
+./keen --generate 100 6dn > keys/normal6.txt &
+./keen --generate 100 7dn > keys/normal7.txt &
+./keen --generate 100 8dn > keys/normal8.txt &
+./keen --generate 100 9dn > keys/normal9.txt &
+
+./keen --generate 100 6dh > keys/hard6.txt &
+./keen --generate 100 7dh > keys/hard7.txt &
+./keen --generate 100 8dh > keys/hard8.txt &
+./keen --generate 100 9dh > keys/hard9.txt &
+
+./keen --generate 100 6dx > keys/extreme6.txt &
+./keen --generate 100 7dx > keys/extreme7.txt &
+./keen --generate 100 8dx > keys/extreme8.txt &
+./keen --generate 100 9dx > keys/extreme9.txt &
