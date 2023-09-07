35e02980e05cfb893d5f0456d98f07124f2a26f5
Saul Spatz <saul.spatz@gmail.com>
2023-09-07 18:07:30 -0500

    Create settings dialog at startup

--- kenken.py
+++ kenken.py
@@ -9,6 +9,7 @@ from control   import Control
 from board     import Board
 from puzzle import Puzzle
 from stopwatch import StopWatch
+from dialogs import PopUp
                                       
 class KenKen(object):            
     def __init__(self, win, height = 1020, width = 1200, cursor = 'crosshair', bg = 'white'):    
@@ -16,16 +17,21 @@ class KenKen(object):
         self.win.title('KenKen')
         icon = tk.Image('photo', file='kenken.png')
         win.tk.call('wm', 'iconphoto', win._w, icon)
-        win.resizable(False, False)        
+        win.resizable(False, False)   
+        self.difficulty = tk.IntVar(self)  # used in the Settings dialog 
+        self.dimension = tk.IntVar(self)     
         self.control = Control(self, win)                
         self.board = Board(self, win, dim = 9, height = height, width = width, bg = bg, cursor=cursor)
         self.timer = StopWatch(win)
         self.timer.pack()
         self.board.pack(side = tk.TOP, expand=tk.YES, fill=tk.BOTH)
+        self.fileSizes = self.getFileSizes()
+        self.settings = PopUp()
+        self.settings.withdraw()
         self.newPuzzle()  # sets self.puzzle
 
     def newPuzzle(self, dim=9, diff='x'):
-        c = run(['./keen', '--generate', '1', f'{dim}d{diff}'], capture_output=True)
+        #c = run(['./keen', '--generate', '1', f'{dim}d{diff}'], capture_output=True)
         code = c.stdout.decode()
         with open('code.txt', 'w') as fin:
             fin.write(code)
@@ -46,6 +52,17 @@ class KenKen(object):
         self.puzzle = Puzzle(self, code)
         self.board.draw(dim)
         self.timer.start()
+
+    def getFileSizes(self):
+        sizes = {}
+        for diff in ('easy', 'normal', 'hard', 'extreme'):
+            for dim in range(6,10):
+                with open(f'keys/{diff}{dim}.txt') as fin:
+                    for count, _ in enumerate(fin):
+                        pass
+                sizes[diff,dim] = count+1
+        return sizes
+
 
                     
 def main():
